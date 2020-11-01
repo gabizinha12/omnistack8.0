b155ce09105a9953ae61878573862db8c0085e57
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-11-01 19:13:54 -0200

    websocket

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const routes = require('./routes')
-const server = express();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
+io.on("connection", (socket) => {
+  console.log("Nova conexão", socket.id);
+});
 
-mongoose.connect('mongodb+srv://omnistack:gabizinha12@cluster0.uxwtd.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://omnistack:gabizinha12@cluster0.uxwtd.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+  }
+);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
